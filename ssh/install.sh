479305a7c4603ad0f0871905d84438075e73387e
PENDY <88583315+Suwidax@users.noreply.github.com>
2024-06-27 18:23:10 +0700

    update

--- ssh/install.sh
+++ ssh/install.sh
@@ -2,10 +2,10 @@
 
 echo "[SSH][Step 1/4] Downloading EX-AutoSCs..."
 
-curl -sS https://raw.githubusercontent.com/bheghenkz/EX-AutoSC/main/ssh/create.sh --output /usr/bin/ssh-create-account
-curl -sS https://raw.githubusercontent.com/bheghenkz/EX-AutoSC/main/ssh/renew.sh --output /usr/bin/ssh-renew-account
-curl -sS https://raw.githubusercontent.com/bheghenkz/EX-AutoSC/main/ssh/password.sh --output /usr/bin/ssh-password-account
-curl -sS https://raw.githubusercontent.com/bheghenkz/EX-AutoSC/main/ssh/delete.sh --output /usr/bin/ssh-delete-account
+curl -sS https://raw.githubusercontent.com/bheghenkz/EX-AutoSC/main/ssh/create.sh --output /usr/local/sbin/ssh-create-account
+curl -sS https://raw.githubusercontent.com/bheghenkz/EX-AutoSC/main/ssh/renew.sh --output /usr/local/sbin/ssh-renew-account
+curl -sS https://raw.githubusercontent.com/bheghenkz/EX-AutoSC/main/ssh/password.sh --output /usr/local/sbin/ssh-password-account
+curl -sS https://raw.githubusercontent.com/bheghenkz/EX-AutoSC/main/ssh/delete.sh --output /usr/local/sbin/ssh-delete-account
 
 echo "[SSH][Step 2/4] EX-AutoSCs has been successfully downloaded"
 
@@ -13,9 +13,9 @@ sleep 1
 
 echo "[SSH][Step 3/4] Applying permission..."
 
-chmod +x /usr/bin/ssh-create-account
-chmod +x /usr/bin/ssh-renew-account
-chmod +x /usr/bin/ssh-password-account
-chmod +x /usr/bin/ssh-delete-account
+chmod +x /usr/local/sbin/ssh-create-account
+chmod +x /usr/local/sbin/ssh-renew-account
+chmod +x /usr/local/sbin/ssh-password-account
+chmod +x /usr/local/sbin/ssh-delete-account
 
 echo "[SSH][Step 4/4] Permission has been successfully applied"
