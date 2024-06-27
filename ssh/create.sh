a80e0de9f0aaa80bacc6ea94e73227caed25aaf7
PENDY <88583315+Suwidax@users.noreply.github.com>
2024-06-27 20:20:22 +0700

    update

--- ssh/create.sh
+++ ssh/create.sh
@@ -6,11 +6,7 @@ EXPIRED_AT=$3
 IPLIMIT="2"
 
 IP=$(curl -s ipv4.icanhazip.com)
-if [[ "$IP" = "" ]]; then
-    DOMAIN=$(cat /etc/xray/domain)
-else
-    DOMAIN=$IP
-fi
+DOMAIN=$(cat /etc/xray/domain)
 
 if [[ $IPLIMIT -gt 0 ]]; then
 mkdir -p /etc/kyt/limit/ssh/ip
@@ -22,18 +18,51 @@ fi
 useradd -e "${EXPIRED_AT}" -s /bin/false -M "${USERNAME}" &> /dev/null
 echo -e "${PASSWORD}\n${PASSWORD}\n" | passwd "${USERNAME}" &> /dev/null
 
-DATADB=$(cat /etc/ssh/.ssh.db | grep "^#ssh#" | grep -w "${USERNAME}" | awk '{print $2}')
-if [[ "${DATADB}" != '' ]]; then
-  sed -i "/\b${USERNAME}\b/d" /etc/ssh/.ssh.db
-fi
-echo "#ssh# ${USERNAME} ${PASSWORD} ${IPLIMIT} ${EXPIRED_AT}" >>/etc/ssh/.ssh.db
+cat > /var/www/html/ssh-$Login.txt <<-END
+=========================
+   SmileVpn Tunneling 
+=========================
+
+Format SSH OVPN Account
+=========================
+Username         : $USERNAME
+Password         : $PASSWORD
+=========================
+IP               : $IP
+Host             : $DOMAIN
+Port OpenSSH     : 443, 80, 22
+Port Dropbear    : 443, 109
+Port Dropbear WS : 443, 109
+Port SSH UDP     : 1-65535
+Port SSH WS      : 80, 8080, 8081-9999
+Port SSH SSL WS  : 443
+Port SSL/TLS     : 400-900
+Port OVPN WS SSL : 443
+Port OVPN SSL    : 443
+Port OVPN TCP    : 1194
+Port OVPN UDP    : 2200
+BadVPN UDP       : 7100, 7300, 7300
+=================================
+Payload WSS: GET wss://BUG.COM/ HTTP/1.1[crlf]Host: $DOMAIN[crlf]Upgrade: websocket[crlf][crlf] 
+=================================
+Payload Enchanced: PATCH / HTTP/1.1[crlf]Host: [host][crlf]Host: bug.com[crlf]Upgrade: websocket[crlf]HTTP/ 3600[crlf]Sec-WebSocket-Extensions: superspeed[crlf]
+=================================
+SSH TLS/SNI : $DOMAIN:443@$USERNAME:$PASSWORD
+SSH Non TLS : $DOMAIN:80@$USERNAME:$PASSWORD
+=================================
+OVPN Download : https://$DOMAIN:81/
+=================================
+Berakhir Pada    : $EXPIRED_AT
+=================================
+
+END
 
 echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 echo "            SSH Account"
 echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-echo "Username    : ${USERNAME}"
-echo "Password    : ${PASSWORD}"
-echo "Expired On  : ${EXPIRED_AT}"
+echo "Username         : ${USERNAME}"
+echo "Password         : ${PASSWORD}"
+echo "Expired On       : ${EXPIRED_AT}"
 echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 echo "IP               : ${IP}"
 echo "Host             : ${DOMAIN}"
@@ -51,7 +80,10 @@ echo "BadVPN UDP       : 7100, 7300, 7300"
 echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 echo "OVPN Download    : https://$DOMAIN:81/"
 echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo "Http Custom Udp  : ${DOMAIN}:1-65535@${USERNAME}:${PASSWORD}"
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 echo "Payload WSS"
 echo "GET wss://isi_bug_disini HTTP/1.1[crlf]Host: ${DOMAIN}[crlf]Upgrade: websocket[crlf][crlf]"
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 echo "Payload WS"
 echo "GET / HTTP/1.1[crlf]Host: ${DOMAIN}[crlf]Upgrade: websocket[crlf][crlf]"
--- ssh/delete.sh
+++ ssh/delete.sh
@@ -5,3 +5,4 @@ USERNAME=$1
 userdel "${USERNAME}" &> /dev/null
 
 rm -rf /etc/kyt/limit/ssh/ip/$USERNAME
+rm -irf /var/www/html/ssh-$USERNAME.txt
