81ec4ffbaedf22b0ecb95fdbe8587a89bea52139
PENDY <88583315+Suwidax@users.noreply.github.com>
2024-06-27 23:18:46 +0700

    Update delete.sh

--- trojan/delete.sh
+++ trojan/delete.sh
@@ -4,7 +4,7 @@ USERNAME=$1
 EXPIRED_AT=$2
 
 sed -i "/^#! ${USERNAME} ${EXPIRED_AT}/,/^},{/d" /etc/xray/config.json
-
+sed -i "/^#! $USERNAME $EXPIRED_AT/,/^},{/d" /etc/xray/config.json
 rm -rf /etc/trojan/$USERNAME
 rm -rf /etc/kyt/limit/trojan/ip/$USERNAME
 rm -rf /etc/limit/trojan/$USERNAME
